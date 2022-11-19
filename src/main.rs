82766f89e3de9e9807f3c1405b3dcbae3d3f6be0
makemeunsee <makemeunsee@gmail.com>
2022-11-20 00:23:05 +0100

    miserable PoC
    egui running on wgpu on hand crafted x11 window; no event loop

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ mod x11;
 use std::{ffi::c_void, iter, time::Duration};
 
 use anyhow::Result;
+use egui::{vec2, Context, Pos2, RawInput};
+use egui_demo_lib::DemoWindows;
+use egui_wgpu_backend::{RenderPass, ScreenDescriptor};
 use raw_window_handle::{
     HasRawDisplayHandle, HasRawWindowHandle, RawDisplayHandle, RawWindowHandle, XcbDisplayHandle,
     XcbWindowHandle,
@@ -25,11 +28,17 @@ struct State {
     queue: wgpu::Queue,
     config: wgpu::SurfaceConfiguration,
     size: (u32, u32),
+    context: Context,
+    raw_input: RawInput,
+    demo_app: DemoWindows,
+    egui_rpass: RenderPass,
 }
 
 impl State {
     fn new(window: &MyWindow) -> Self {
         let size = (window.width, window.height);
+
+        // wgpu stuff
 
         // The instance is a handle to our GPU
         // BackendBit::PRIMARY => Vulkan + Metal + DX12 + Browser WebGPU
@@ -62,12 +71,39 @@ impl State {
         };
         surface.configure(&device, &config);
 
+        // egui stuff
+
+        let scale_factor = 2.;
+        let raw_input = egui::RawInput {
+            screen_rect: Some(egui::Rect::from_min_size(
+                Pos2::new(50., 50.),
+                vec2(size.0 as f32 - 100., size.1 as f32 - 100.) / scale_factor,
+            )),
+            pixels_per_point: Some(scale_factor),
+            ..Default::default()
+        };
+
+        let surface_format = surface.get_supported_formats(&adapter)[0];
+        // We use the egui_wgpu_backend crate as the render backend.
+        let egui_rpass = RenderPass::new(&device, surface_format, 1);
+
+        // Display the demo application that ships with egui.
+        let demo_app = egui_demo_lib::DemoWindows::default();
+
+        let context = Context::default();
+        // context.set_fonts(_);
+        // context.set_style(_);
+
         Self {
             surface,
             device,
             queue,
             config,
             size,
+            context,
+            raw_input,
+            demo_app,
+            egui_rpass,
         }
     }
 
@@ -80,42 +116,67 @@ impl State {
         }
     }
 
-    fn update(&mut self) {}
-
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
-        let output = self.surface.get_current_texture()?;
-        let view = output
+        let output_frame = self.surface.get_current_texture().unwrap();
+        let output_view = output_frame
             .texture
             .create_view(&wgpu::TextureViewDescriptor::default());
+
+        // Begin to draw the UI frame.
+        let scale_factor = self.raw_input.pixels_per_point.unwrap_or(1.);
+        self.context.begin_frame(self.raw_input.take());
+        self.raw_input.pixels_per_point = Some(scale_factor);
+
+        // Draw the demo application.
+        self.demo_app.ui(&self.context);
+
+        // End the UI frame. We could now handle the output and draw the UI with the backend.
+        let full_output = self.context.end_frame();
+        let paint_jobs = self.context.tessellate(full_output.shapes);
 
         let mut encoder = self
             .device
             .create_command_encoder(&wgpu::CommandEncoderDescriptor {
-                label: Some("Render Encoder"),
+                label: Some("encoder"),
             });
 
-        {
-            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
-                label: Some("Render Pass"),
-                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
-                    view: &view,
-                    resolve_target: None,
-                    ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(wgpu::Color {
-                            r: 0.1,
-                            g: 0.2,
-                            b: 0.3,
-                            a: 0.5,
-                        }),
-                        store: true,
-                    },
-                })],
-                depth_stencil_attachment: None,
-            });
-        }
+        // Upload all resources for the GPU.
+        let screen_descriptor = ScreenDescriptor {
+            physical_width: self.config.width,
+            physical_height: self.config.height,
+            scale_factor,
+        };
+        let tdelta: egui::TexturesDelta = full_output.textures_delta;
+        self.egui_rpass
+            .add_textures(&self.device, &self.queue, &tdelta)
+            .expect("add texture ok");
+        self.egui_rpass
+            .update_buffers(&self.device, &self.queue, &paint_jobs, &screen_descriptor);
 
+        // Record all render passes.
+        self.egui_rpass
+            .execute(
+                &mut encoder,
+                &output_view,
+                &paint_jobs,
+                &screen_descriptor,
+                Some(wgpu::Color {
+                    r: 0.2,
+                    g: 0.1,
+                    b: 0.3,
+                    a: 0.2,
+                }),
+            )
+            .unwrap();
+        // Submit the commands.
         self.queue.submit(iter::once(encoder.finish()));
-        output.present();
+
+        // Redraw egui
+        output_frame.present();
+
+        self.egui_rpass
+            .remove_textures(tdelta)
+            .expect("remove texture ok");
 
         Ok(())
     }
@@ -138,8 +199,6 @@ unsafe impl HasRawDisplayHandle for MyWindow {
     }
 }
 
-// TODO try egui on top: https://github.com/hasenbanck/egui_example/blob/master/src/main.rs
-
 fn main() -> Result<()> {
     let (conn, screen_num) = x11rb::xcb_ffi::XCBConnection::connect(None)?;
 
@@ -150,10 +209,10 @@ fn main() -> Result<()> {
     let win_id = create_overlay_window(
         &conn,
         screen,
-        50,
-        50,
-        screen.width_in_pixels - 100,
-        screen.height_in_pixels - 100,
+        100,
+        100,
+        screen.width_in_pixels - 200,
+        screen.height_in_pixels - 200,
     )?;
 
     conn.map_window(win_id)?;
@@ -164,8 +223,8 @@ fn main() -> Result<()> {
         visual_id: screen.root_visual,
         connection: conn.get_raw_xcb_connection(),
         screen: screen_num as i32,
-        width: screen.width_in_pixels as u32 - 100,
-        height: screen.height_in_pixels as u32 - 100,
+        width: screen.width_in_pixels as u32 - 200,
+        height: screen.height_in_pixels as u32 - 200,
     };
 
     let mut state = State::new(&window);
